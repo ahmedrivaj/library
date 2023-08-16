d1e6599d73e38b450fbf981f0b0f2d8e614bdc61
احمد رواج <rivaj@fpa.office>
2023-08-16 14:33:28 +0500

    starting e1

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BooksController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('/books', [BooksController::class, 'store']);
+
+Route::patch('/books/{book}', [BooksController::class, 'update']);
